36b4c4cef45ba1e1a0617a6a94876c51199fed58
dogefromage <seb.sa@eblcom.ch>
2021-08-11 21:34:01 +0200

    tested more features

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,4 +1,3 @@
-
 import * as quick from 'quickio';
 
 let game = new quick.Game();
@@ -8,22 +7,48 @@ let ctx = canvas?.getContext('2d');
 
 let player = game.addEntity();
 
-let transform = <quick.Transform2d>player.addComponent(quick.Transform2d);
-transform.position.xy = [ 300, 200 ];
-
-let renderer2d = <quick.Renderer2d>player.addComponent(quick.Renderer2d);
-renderer2d.addRenderRule((ctx) =>
+class PlayerStuff extends quick.Component
 {
-    ctx.fillStyle = '000000';
-    ctx.fillRect(transform.position.x, transform.position.y, 20, 20);
-});
+    private transform: quick.Transform2d;
+    private input: quick.InputChannel;
 
-requestAnimationFrame(() =>
+    start()
+    {
+        this.transform = <quick.Transform2d>this.entity.getComponent(quick.Transform2d);
+        this.transform.position.xy = [ 300, 400 ];
+        
+        let renderer = <quick.Renderer2d>this.entity.getComponent(quick.Renderer2d);
+        renderer.addRenderStep((ctx) =>
+        {
+            ctx.fillStyle = '#ffffff';
+            ctx.fillRect(0, 0, 600, 600);
+            
+            ctx.fillStyle = '#000000';
+            ctx.fillRect(this.transform.position.x, this.transform.position.y, 20, 20);
+        });
+
+        this.input = this.game.createInputChannel();
+    }
+
+    update()
+    {
+        let xAxis = this.input.getKeyDownTime('KeyD') - this.input.getKeyDownTime('KeyA');
+        let yAxis = this.input.getKeyDownTime('KeyS') - this.input.getKeyDownTime('KeyW');
+
+        this.transform.position.x += 100 * xAxis;
+        this.transform.position.y += 100 * yAxis;
+    }
+}
+player.addComponent(PlayerStuff);
+
+function update()
 {
     game.update();
-
+    
     if (ctx)
     {
         game.render(ctx);
     }
-});
+    requestAnimationFrame(update);
+}
+requestAnimationFrame(update);
